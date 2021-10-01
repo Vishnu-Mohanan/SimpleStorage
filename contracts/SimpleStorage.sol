a1e8fe8aeb0e991783085cc89e77ab03bb8f2c15
"Vishnu Mohanan" <"mohanvishnu4699@gmail.com">
2021-10-01 20:49:37 +0530

    new

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.6.0 < 0.9.0;
+
+contract SimpleStorage{
+    
+    uint256 public favNumber;
+    
+    function store (uint256 _favNumber) public{
+        favNumber = _favNumber;
+    }
+}
