0c0d6a3e9c36dd455ba6fb52ddab13aac6161bb3
"Vishnu Mohanan" <"mohanvishnu4699@gmail.com">
2021-11-04 19:15:52 +0530

    "Added a comment after cloning"

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,6 +3,8 @@ pragma solidity >= 0.6.0 < 0.9.0;
 
 contract SimpleStorage{
     
+    // Trying from remix
+    
     uint256 public favNumber;
     
     function store (uint256 _favNumber) public{
